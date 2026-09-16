d4ee3098bdfb99aa83dadb28d735848463cacbc7
Sonia Guadalupe Garcia <sonia.guadalupe.garcia@gmail.com>
2026-09-16 06:55:04 -0600

    Fix

--- src/test/java/com/buyio/audit/BuyioAuditServiceApplicationTests.java
+++ src/test/java/com/buyio/audit/BuyioAuditServiceApplicationTests.java
@@ -1,13 +1,12 @@
-package com.exam.audit_service;
+package com.buyio.audit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuditServiceApplicationTests {
+class BuyioAuditServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
